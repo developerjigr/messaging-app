7a3016d8252e0cc8c282d352c714df217f4855b8
developerjigr <jigar.polra@icloud.com>
2019-10-10 12:57:12 +0100

    pushing local changes

---
+++ messaging interface demo/Features/View Controllers/Conversation List/ConversationListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ConversationListViewController.swift
+//  messaging interface demo
+//
+//  Created by Jigar Polra on 26/09/2019.
+//  Copyright © 2019 JIG_R. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversationListViewController: UIViewController {
+
+	@IBOutlet var conversationListTableView: UITableView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+
+}
